3ea947a7bb4b06a2eb78dcdecd28b07f6d501bc0
Lahuts <albumcroquis@gmail.com>
2022-10-21 11:17:15 +0200

    test

--- TranspositionInverse.java
+++ TranspositionInverse.java
@@ -9,11 +9,23 @@ public class TranspositionInverse implements ProtocoleChiffremment
 	@Override
 	public String chiffre(String message) 
 	{
+		String me = "";
+		String[] m = message.split("");
+		for(int i = m.length; i > 0; i--){
+			me += m[i];
+		}
+		return me;
 	}
 
 	@Override
 	public String dechiffre(String message) 
 	{
+		String me = "";
+		String[] m = message.split("");
+		for(int i = 0; i < m.length; i++){
+			me += m[i];
+		}
+		return me;
 	}
 	
 }
